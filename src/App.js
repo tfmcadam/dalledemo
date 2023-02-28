fbd1af9bbd0145e6bb246e10b85cc2a1a91cac43
tfmcadam <emergin25@gmail.com>
2023-02-27 16:54:37 -0800

    css changes and created a Route

--- src/App.js
+++ src/App.js
@@ -1,47 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-const { Configuration, OpenAIApi, } = require("openai")
-const configuaration = new Configuration({
-  apiKey: process.env.REACT_APP_API_KEY,
-});
-const openai = new OpenAIApi(configuaration);
+import { DalleDemo } from './views/DalleDemo'
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [userPrompt, setUserPrompt] = useState("")
-  const [imageUrl, setImageUrl] = useState("")
-
-  const generateImage = async () => {
-    const imageParameters = {
-      prompt: userPrompt,
-      n: 1,
-      size: "1024x1024",
-    }
-    const response = await openai.createImage(imageParameters);
-    const urlData = response.data.data[0].url
-    console.log(urlData);
-    setImageUrl(urlData);
-  }
+  
+  
   return (
     <div className="App">
-      <h1>Generate a unique image With Dall-E based on what you want to See</h1>
-      <p>Get creative and see what you get!</p>
-      <div className='shadowBox'>
-        {
-          imageUrl
-            ? <img className="image" src={imageUrl} alt="ai thing" />
-            : <img src={logo} className="App-logo" alt="logo" />
-        }
+      <nav>
+        <a href="/dalleII"></a>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Navigate to='/dalleII' replace/>}/>
+        <Route path="/dalleII" element={<DalleDemo/>}/>
+      </Routes>
       </div>
-      <div className='flex'>
-        <input
-          placeholder='Crystal sitting in a chair, looking at her phone'
-          onChange={(e) => setUserPrompt(e.target.value)}
-        />
-        <button className='btn' onClick={() => generateImage()}>Generate</button>
-      </div>
-    </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/views/DalleDemo.jsx
@@ -0,0 +1,62 @@
+import logo from '../logo.svg';
+// import './App.css';
+import { useEffect, useState } from 'react';
+const { Configuration, OpenAIApi, } = require("openai")
+const configuaration = new Configuration({
+    apiKey: process.env.REACT_APP_API_KEY,
+});
+const openai = new OpenAIApi(configuaration);
+
+export const DalleDemo = () => {
+
+    const [userPrompt, setUserPrompt] = useState("")
+    const [imageUrl, setImageUrl] = useState("")
+
+    const generateImage = async () => {
+        const imageParameters = {
+            prompt: userPrompt,
+            n: 1,
+            size: "1024x1024",
+        }
+        const response = await openai.createImage(imageParameters);
+        const urlData = response.data.data[0].url
+        console.log(urlData);
+        setImageUrl(urlData);
+    }
+    return (
+        <div className="App">
+            <h1>Generate a unique image With Dall-E based on what you want to See</h1>
+            <p className='margin-bottom'>Get creative and see what you get!</p>
+            <div className='shadowBox'>
+                {
+                    imageUrl
+                        ? <img className="image" src={imageUrl} alt="ai thing" />
+                        : <img src={logo} className="App-logo" alt="logo" />
+                }
+            </div>
+            <div className='flex'>
+                {/* <input
+        placeholder='Crystal sitting in a chair, looking at her phone'
+        onChange={(e) => setUserPrompt(e.target.value)}
+        /> */}
+                <textarea className="width margin-bottom" rows="4" col="200" onChange={(e) => setUserPrompt(e.target.value)}
+                    placeholder='Add your image idea here...'></textarea>
+            </div>
+            <button className='btn' onClick={() => generateImage()}>Generate</button>
+            {
+                imageUrl ? (
+                    <div>
+                        <h3>Prompt:</h3>
+                        <p>{userPrompt}</p>
+                        <h3>Image URL (copy/paste): </h3>
+                        <p className='width'>{imageUrl}</p>
+                    </div>
+
+                ) : (
+
+                    <p></p>
+                )}
+        </div>
+    );
+}
+
