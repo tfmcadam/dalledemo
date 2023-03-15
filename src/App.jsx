394b73056fa464b006bf66c7a3ddccbb974d7780
tfmcadam <emergin25@gmail.com>
2023-03-14 17:03:37 -0700

    removed NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
 import './App.css';
-import { Modal } from './modal/Modal';
-import { useState } from 'react';
+import { NavBar } from './components/NavBar';
 import { DalleDemo } from './views/DalleDemo'
 import { ChatDemo } from './views/ChatDemo';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [show, setShow] = useState(false);
 
   return (
     <div className="App">
-      <nav className='topnav'>
-        <ul>
-          <li><Link to="/">Chat Demo</Link></li>
-          <li><Link to="/dalleII"> Dalle Demo</Link></li>
-          <li className="float-right">
-            <button className="open-btn" onClick={() => setShow(true)}>Contact Me</button>
-            <Modal onClose={() => setShow(false)} show={show}/></li>
-        </ul>
-      </nav>
+      <NavBar/>
+      
 
       <Routes>
-        <Route path="/" element={<ChatDemo/>} />
-        <Route path="/dalleII" element={<DalleDemo/>} />
+        <Route path="/" element={<DalleDemo/>} />
+        <Route path="/chat" element={<ChatDemo/>} />
       </Routes>
     </div>
   )
