b4c4a0cc5d08bb058f53a6c9677094acee5d0d06
tfmcadam <emergin25@gmail.com>
2023-03-14 17:02:51 -0700

    Added responsive NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,49 @@
+import "./NavBar.css"
+import { Link } from "react-router-dom";
+import { Modal } from "../modal/Modal";
+import React, { useState, useEffect } from "react";
+
+export const NavBar = () => {
+
+    const [show, setShow] = useState(false);
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+    useEffect(() => {
+
+        const changeWidth = () => {
+            setScreenWidth(window.innerWidth);
+        }
+
+        window.addEventListener('resize', changeWidth)
+
+        return () => {
+            window.removeEventListener('resize', changeWidth)
+        }
+
+    }, [])
+
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu)
+    }
+
+    return (
+
+        <nav >
+            <h1 className="title"> OpenAi Fun</h1>
+            {(toggleMenu || screenWidth > 700) &&
+                (
+                    <ul className="list">
+                        
+                        <li className="items"><Link to="/">Dalle Demo</Link></li>
+                        <li className="items"><Link to="/chat"> Chat Demo</Link></li>
+                        <li className="items">
+                            <button className="open-btn " onClick={() => setShow(true)}>Contact Me</button>
+                            <Modal onClose={() => setShow(false)} show={show} /></li>
+                    </ul>
+                )}
+            <button onClick={toggleNav} className="nav-btn"><i class="fa-sharp fa-solid fa-bars"></i></button>
+
+        </nav>
+    )
+}
